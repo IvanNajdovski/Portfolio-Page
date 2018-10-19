eb4f958de32252e42496fc046486f49530006023
IvanNajdovski <ivan_najdovski@hotmail.com>
2018-10-19 12:40:46 +0200

    1

--- js/index.js
+++ js/index.js
@@ -170,13 +170,16 @@ $(document).ready(function () {
             $(".project__details").text("Page is using three API to collect Weather, Geolocation and Photos data and is giving the user information for the weather of the given location. The application is using Axios NodeJs library to call the API's and EJS for template rendering. Also favorite location is best done with database but for investigating the localStorage sistem we used localStorage instead.");
         }else if(index === 2){
             $(".project__name").text("Burke - Movie Site");
-            $(".project__details").text("'Burke' is a Movie Company. This site has very good animations");
+            $(".project__details").text("'Burke' Website has very good animations and very good wellcome screen, we used alot of animations and Javascript to tie them all together. SVG logo was one of thigs learned while making this site wich gives us a very good perspective and imagination about what else can be done with SVG.");
         }else if(index === 3){
             $(".project__name").text("Build In Amsterdam");
+            $(".project__details").text("Portfolio page where bunch of projects and rendered side by side. Horisontal scroll was a bit challenging but gives a great UX experience. Site renderes different data for different project and use of different number of projects gives the same result.")
         }else if(index === 4){
             $(".project__name").text("Localised");
+            $(".project__details").text("WebPage for Company that makes web-pages, tons of great effects and animations that gives this site very modern look and great UX design, fully responsive witch gives all users great experience. Work with sudo elements poved very fruitful");
         }else if(index === 5){
             $(".project__name").text("Charlie Paris Watch-Shop");
+            $(".project__details").text("Shopping page for watch Company where we made data sistem to mimic the database usage where the user gets the same item when redirected to another page, for this feature we used URL params to pass variables and arrays in order to render the right data to the pages. Loads of animations and sudo elements working with Javascript that give us a taste of what Javascript can do and it can do everything we want.");
         }
 
         setTimeout(function(){
@@ -196,13 +199,47 @@ $(document).ready(function () {
         var y = e.pageY;
         var top = $(".projects__content__photos").offset().top;
         var left = $(".projects__content__photos").offset().left;
-        offsetX = (x - (left + ($(".projects__content__photos").outerWidth()/2)))/40;
-        offsetY = (y - (top + ($(".projects__content__photos").outerHeight()/2)))/20;
+        offsetX = (x - (left + ($(".projects__content__photos").outerWidth()/2)))/50;
+        offsetY = (y - (top + ($(".projects__content__photos").outerHeight()/2)))/30;
         console.log("top is",offsetY,"left is", offsetX)
 
 
         $(".projects__content__photos").css("translation","transform .5s")
         $(".projects__content__photos").css("transform",` translateY(-100px) rotateX(${offsetY}deg) rotateY(${offsetX}deg)`);
+        $(".projects__content__photos-wrapper").css("transform",`rotateX(${offsetY}deg) rotateY(${offsetX}deg)`);
+
+    });
+    var count;
+    var counter = 0
+    $(".skill").on("mouseover", function() {
+        $(this).children("div").children("img").addClass("active");
+        $(this).find(".under").addClass("active");
+    });
+    $(".skill").on("mouseover", function(){
+        var percent = $(this).find(".under");
+        if(percent.attr("aria-label")){
+            setTimeout(function(){
+
+
+            var count = setInterval(function(){
+                counter++
+                percent.text(counter + "%")
+                if(counter >= Number(percent.attr("aria-label"))){
+                    clearInterval(count)
+                }
+            },25)
+            },300)
+        }
+
+    });
+
+    $(".skill").on("mouseleave", function(){
+        $(this).children("div").children("img").removeClass("active");
+        $(this).find(".under").removeClass("active");
+        counter = 0
+        clearInterval(count)
+
+
 
     })
 
